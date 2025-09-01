a6936bdbabe180cb27adf4bc75a9593ee2d983f7
cyf8 <2308016738@qq.com>
2025-09-01 20:49:36 +0800

    Update index.js

--- 25夏《移动软件开发》/实验2：实时天气查询/index.js
+++ 25夏《移动软件开发》/实验2：实时天气查询/index.js
@@ -1,74 +1,70 @@
-// pages/index/index.js
 Page({
+  /**
+  * 页面的初始数据
+  */
   data: {
-    region: ['山东省', '青岛市', '黄岛区'], // 默认地区
-    location: '山东省青岛市黄岛区', // 地理位置
-    id: 101120206, // 城市ID
-    now: '' // 当前天气信息
-  },
-
-  /*更改地区*/
-  regionChange: function(e) {
-    this.setData({
-      region: e.detail.value // 更新地区数据
-    });
-    this.getWeather();
-  },
-  /*获取城市的ID */
-  getLocationId: function() {
-    let that = this;
-    wx.request({
-      url: 'https://mg78kya6kc.re.qweatherapi.com/geo/v2/city/lookup',
-      data: {
-        location: that.data.region, 
-        key: 'ce9f979d8b7940719fa93eed1cf16ba6' 
-      },
-
-      success(res) {
-        console.log(res.data.location[0].id)
-        that.setData({
-          id: res.data.location[0].id 
-        });
+      region:['安徽省','芜湖市','镜湖区'],
+      now:{
+      tmp:0,
+      cond_txt:'未知',
+      cond_code:'999',
+      hum:0,
+      pres:0,
+      vis:0,
+      wind_dir:0,
+      wind_spd:0,
+      wind_sc:0
       }
-    });
   },
 
-  // 获取当前天气信息
-  getWeather: function() {
-    let that = this;
-    that.getLocationId();
-    wx.request({
-      url: 'https://mg78kya6kc.re.qweatherapi.com/v7/weather/now',
-      data: {
-        location: that.data.id, 
-        key: 'ce9f979d8b7940719fa93eed1cf16ba6' 
-      },
-      success(res) {
-        console.log(res.data.now)
-        that.setData({
-          now: res.data.now 
-        });
-      }
-    });
-  },
-
-  // 页面加载时调用
-  onLoad(options) {
+  /**
+  * 更新省、市、 区信息
+  */
+  regionChange:function(e){
+      this.setData({region:e.detail.value});
       this.getWeather();
-
   },
 
-  onReady() {}, // 页面初次渲染完成时调用
+  /**
+  * 获取实况天气数据
+  */
+  getWeather:function(){
+      var that = this;
+      
+      // 先通过地名获取 Location ID
+      wx.request({
+      url: 'https://mt4y3jhkp4.re.qweatherapi.com/geo/v2/city/lookup',
+      data: {
+          location: that.data.region[1],  // 使用地名
+          key: '0d42f9fa16734b6bac2dc57d6ee96f9c'
+      },
+      success: function(res) {
+          // 获取 Location ID
+          var locationId = res.data.location[0].id;
+          
+          // 使用 Location ID 查询天气
+          wx.request({
+          url: 'https://mt4y3jhkp4.re.qweatherapi.com/v7/weather/now',
+          data: {
+              location: locationId,
+              key: '0d42f9fa16734b6bac2dc57d6ee96f9c',
+              adm: that.data.region[1] // 使用地名
+          },
+          success: function(res) {
+              console.log(res.data);
+              that.setData({now:res.data.now});
+          }
+          })
+      }
+      });
+  },
 
-  onShow() {}, // 页面显示时调用
 
-  onHide() {}, // 页面隐藏时调用
+  /**
+  * 生命周期函数--监听页面加载
+  */
+  onLoad: function (options) {
+      this.getWeather();
+  },
+})
 
-  onUnload() {}, // 页面卸载时调用
-
-  onPullDownRefresh() {}, // 用户下拉动作时调用
-
-  onReachBottom() {}, // 页面上拉触底事件的处理函数
-
-  onShareAppMessage() {} // 用户点击右上角分享时调用
-});
